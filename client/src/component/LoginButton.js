c873171d85b66ee78626435b4554f601b664a960
bkhrm <snuar614@gmail.com>
2023-08-17 13:56:44 +0900

    로그인 버튼, 회원가입 버튼 수정

--- client/src/component/LoginButton.js
+++ client/src/component/LoginButton.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import "./LoginButton.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginButton() {
+
+	const navigate = useNavigate();
+
 	return (
 		<button
 			id="loginBtn"
-			type="button">
-			<Link to="/login">Log in</Link>
-		</button>
+			type="button"
+			onClick={()=>{navigate('/login')}}>Log in</button>
 	);
 }
 
--- client/src/component/SignUpButton.js
+++ client/src/component/SignUpButton.js
@@ -2,13 +2,16 @@ import "./SignUpButton.css";
 import { Link } from "react-router-dom";
 
 function SignUpButton({ loginState }) {
-	return (
-		<button
-			id="signup"
-			type="button">
-			{loginState ? <Link to="/">Log out</Link> : <Link to="/signup">Sign up</Link>}
-		</button>
-	);
+    return (
+        <Link to={loginState ? "/" : "/signup"}>
+            <button
+                id="signup"
+                type="button">
+                {loginState ? "Log out" : "Sign up"}
+            </button>
+        </Link>
+    );
 }
 
 export default SignUpButton;
+
