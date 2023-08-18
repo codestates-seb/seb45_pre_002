29813276a6e035f7a6ad24ba49b929f5fb98a368
leesaewoo <ljww123@naver.com>
2023-08-18 15:52:18 +0900

    feat: #45 #46 멤버와 코멘트의 엔티티에 연관관계 매핑 추가

--- server/src/main/java/com/codestates/stackoverflow/questioncomment/mapper/QuestionCommentMapper.java
+++ server/src/main/java/com/codestates/stackoverflow/questioncomment/mapper/QuestionCommentMapper.java
@@ -1,7 +1,35 @@
 package com.codestates.stackoverflow.questioncomment.mapper;
 
+import com.codestates.stackoverflow.question.entity.Questions;
+import com.codestates.stackoverflow.questioncomment.dto.QuestionCommentDto;
+import com.codestates.stackoverflow.questioncomment.entity.QuestionComment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QuestionCommentMapper {
+
+    public QuestionComment postToQuestionComment(QuestionCommentDto.PostDto postDto) {
+        if(postDto == null) {
+            return null;
+        }
+        else {
+            QuestionComment questionComment = new QuestionComment();
+            questionComment.setBody(postDto.getBody());
+            questionComment.setQuestion(postToQuestion(postDto));
+
+            return questionComment;
+        }
+    }
+
+    private Questions postToQuestion(QuestionCommentDto.PostDto postDto) {
+        if(postDto == null) {
+            return null;
+        }
+        else {
+            Questions question = new Questions();
+            //question.setQuestionId(postDto.getQuestionId());
+
+            return question;
+        }
+    }
 }
